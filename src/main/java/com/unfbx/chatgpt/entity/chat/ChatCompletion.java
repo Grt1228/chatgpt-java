03fe2adb839f1ca082d7f12837ebe43660b8a96f
pt <plexpt@gmail.com>
2023-03-19 14:29:50 +0800

    gpt4

--- src/main/java/com/unfbx/chatgpt/entity/chat/ChatCompletion.java
+++ src/main/java/com/unfbx/chatgpt/entity/chat/ChatCompletion.java
@@ -179,6 +179,10 @@ public class ChatCompletion implements Serializable {
     public enum Model {
         GPT_3_5_TURBO("gpt-3.5-turbo"),
         GPT_3_5_TURBO_0301("gpt-3.5-turbo-0301"),
+        GPT_4("gpt-4"),
+        GPT_4_0314("gpt-4-0314"),
+        GPT_4_32K("gpt-4-32k"),
+        GPT_4_32K_0314("gpt-4-32k-0314"),
         ;
         private String name;
     }
