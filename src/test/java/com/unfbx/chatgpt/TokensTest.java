e4991ddf773041a9c95a223826c937740d0e67a6
guorutao <285012>
2023-04-05 01:12:26 +0800

    feature:1.0.10 支持tokens计算

--- src/test/java/com/unfbx/chatgpt/TokensTest.java
+++ src/test/java/com/unfbx/chatgpt/TokensTest.java
@@ -6,7 +6,9 @@ import com.knuddels.jtokkit.api.Encoding;
 import com.knuddels.jtokkit.api.EncodingRegistry;
 import com.knuddels.jtokkit.api.EncodingType;
 import com.knuddels.jtokkit.api.ModelType;
+import com.unfbx.chatgpt.entity.chat.ChatCompletion;
 import com.unfbx.chatgpt.entity.chat.Message;
+import com.unfbx.chatgpt.entity.completions.Completion;
 import com.unfbx.chatgpt.utils.TikTokensUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
@@ -39,6 +41,43 @@ public class TokensTest {
         message.add(Message.builder().role(Message.Role.USER).content("关注微信公众号：程序员的黑洞。").build());
         message.add(Message.builder().role(Message.Role.USER).content("进入chatgpt-java交流群获取最新版本更新通知。").build());
         TikTokensUtil.tokens(enc, message);
+        log.info("Message集合文本：【{}】", message, tokens);
+        log.info("总tokens数{}", tokens);
+    }
+
+    @Test
+    public void byEncodingTypeTest() throws JsonProcessingException {
+        String text = "关注微信公众号：程序员的黑洞。进入chatgpt-java交流群获取最新版本更新通知。";
+        List<Integer> encode = TikTokensUtil.encode(EncodingType.CL100K_BASE, text);
+        log.info(encode.toString());
+        long tokens = TikTokensUtil.tokens(EncodingType.CL100K_BASE, text);
+        log.info("单句文本：【{}】", text);
+        log.info("总tokens数{}", tokens);
+        log.info("--------------------------------------------------------------");
+        List<Message> message = new ArrayList<>(2);
+        message.add(Message.builder().role(Message.Role.USER).content("关注微信公众号：程序员的黑洞。").build());
+        message.add(Message.builder().role(Message.Role.USER).content("进入chatgpt-java交流群获取最新版本更新通知。").build());
+        TikTokensUtil.tokens(EncodingType.CL100K_BASE, message);
+        log.info("Message集合文本：【{}】", message, tokens);
+        log.info("总tokens数{}", tokens);
+    }
+
+
+    @Test
+    public void byModelNameTest() throws JsonProcessingException {
+        String text = "关注微信公众号：程序员的黑洞。进入chatgpt-java交流群获取最新版本更新通知。";
+//        String modelName = ChatCompletion.Model.GPT_3_5_TURBO.getName();
+        String modelName = Completion.Model.DAVINCI_003.getName();
+        List<Integer> encode = TikTokensUtil.encode(modelName, text);
+        log.info(encode.toString());
+        long tokens = TikTokensUtil.tokens(modelName, text);
+        log.info("单句文本：【{}】", text);
+        log.info("总tokens数{}", tokens);
+        log.info("--------------------------------------------------------------");
+        List<Message> message = new ArrayList<>(2);
+        message.add(Message.builder().role(Message.Role.USER).content("关注微信公众号：程序员的黑洞。").build());
+        message.add(Message.builder().role(Message.Role.USER).content("进入chatgpt-java交流群获取最新版本更新通知。").build());
+        TikTokensUtil.tokens(modelName, message);
         log.info("Message集合文本：【{}】", message, tokens);
         log.info("总tokens数{}", tokens);
     }
