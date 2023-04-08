0cb60dbac7b552b462a68814a9a5e49c58331996
guorutao <285012>
2023-04-08 18:09:35 +0800

    feature:1.0.11 支持tokens BUG修复

--- src/main/java/com/unfbx/chatgpt/entity/images/Item.java
+++ src/main/java/com/unfbx/chatgpt/entity/images/Item.java
@@ -1,5 +1,6 @@
 package com.unfbx.chatgpt.entity.images;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -13,4 +14,6 @@ import java.io.Serializable;
 @Data
 public class Item implements Serializable {
     private String url;
+    @JsonProperty("b64_json")
+    private String b64Json;
 }
