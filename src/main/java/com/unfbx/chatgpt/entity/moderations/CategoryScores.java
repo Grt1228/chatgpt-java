232f3db50a999900fffd9d7448b8e6a0eaa7cd42
guorutao <285012>
2023-07-07 23:23:12 +0800

    feature 1.0.15 Moderation接口返回值更新

--- src/main/java/com/unfbx/chatgpt/entity/moderations/CategoryScores.java
+++ src/main/java/com/unfbx/chatgpt/entity/moderations/CategoryScores.java
@@ -28,4 +28,24 @@ public class CategoryScores implements Serializable {
     @JsonProperty("violence/graphic")
     private BigDecimal violenceGraphic;
 
+    /**
+     * 对任何目标表达、煽动或宣扬骚扰性语言的内容。
+     */
+    @JsonProperty("harassment")
+    private BigDecimal harassment;
+    /**
+     * 骚扰内容还包括对任何目标的暴力或严重伤害。
+     */
+    @JsonProperty("harassment/threatening")
+    private BigDecimal harassmentThreatening;
+    /**
+     * 说话者表示他们正在或打算进行自残行为的内容，例如自杀、割伤和饮食失调。
+     */
+    @JsonProperty("self-harm/intent")
+    private BigDecimal selfHarmIntent;
+    /**
+     * 鼓励进行自残行为（例如自杀、割伤和饮食失调）的内容，或者提供有关如何实施此类行为的说明或建议的内容。
+     */
+    @JsonProperty("self-harm/instructions")
+    private BigDecimal selfHarmInstructions;
 }
